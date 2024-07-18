2df54734f4f656cd6eeb6d6f1cb1b5c4088b62ba
Gonmaro <148899037+gon-maro@users.noreply.github.com>
2024-07-19 02:29:55 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 //メニューのやつ
-$(function() {  $(".A").click(function() {  $(".B").toggleClass("C");  });});
+$(function() {  
+    $(".A").click(function() 
+        {  $(".B").toggleClass("C");  
+
+        });
+    });
 
 
 //スクロール1//
@@ -11,7 +16,14 @@ const scroll_news = function () {
     });
 };
 const scroll_detayo1 = function () {
-    const top = document.getElementById("detayo1").getBoundingClientRect().top;
+    const top = document.getElementById("points").getBoundingClientRect().top;
+    window.scrollTo({
+        top: top,
+        behavior: 'smooth'
+    });
+};
+const scroll_other = function () {
+    const top = document.getElementById("contents").getBoundingClientRect().top;
     window.scrollTo({
         top: top,
         behavior: 'smooth'
